4432c68b122a92d1f8c04e18ba7fcbc398fa0f19
vitaly <vitaly200277@mail.ru>
2020-11-30 17:51:18 +0300

    start web

---
+++ www/sc.js
@@ -0,0 +1,16 @@
+﻿let num = 0;
+QRScan.onclick = function () {
+    window.nativeAppFacade.startQR();  
+}
+
+Share.onclick = function () {
+    window.nativeAppFacade.startShare("titile", "vk.com");
+}
+
+class MobileAppFacade {
+    static resultQR(qr) {
+        label.textContent = qr;
+    }
+}
+
+MobileAppFacade.resultQR("label");
